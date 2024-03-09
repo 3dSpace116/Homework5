b8a30b68d953798d69ecb0e747384421d0df624c
3dSpace116 <mirskam@gmail.com>
2024-03-09 09:38:04 +0300

    Pull request

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,89 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    //Задача 1
+        int clientOS=0;
+
+        if (clientOS == 0) {System.out.println("Установите версию приложения для iOS по ссылке");}
+        else {System.out.println("Установите версию приложения для Android по ссылке");}
+
+    //Задача 2
+        int clientDeviceYear=2013;
+
+        if (clientDeviceYear>=2015) {
+            if (clientOS == 0) {System.out.println("Установите версию приложения для iOS по ссылке");}
+            else {System.out.println("Установите версию приложения для Android по ссылке");}}
+
+        else {
+            if (clientOS == 0) {System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
+            else {System.out.println("Установите блегченную версию приложения для Android по ссылке");}}
+
+    // то же самое через 4 строки в 1 этажность
+        if (clientDeviceYear>=2015 && clientOS == 0) {System.out.println("Установите версию приложения для iOS по ссылке");}
+        if (clientDeviceYear>=2015 && clientOS == 1) {System.out.println("Установите версию приложения для Android по ссылке");}
+        if (clientDeviceYear<2015 && clientOS == 0) {System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
+        if (clientDeviceYear<2015 && clientOS == 1) {System.out.println("Установите блегченную версию приложения для Android по ссылке");}
+
+    // тернарный, не работает, java: illegal start of expression
+
+    //    clientDeviceYear>=2015
+    //            ? if (clientOS == 0) {System.out.println("Установите версию приложения для iOS по ссылке");}
+    //              else {System.out.println("Установите версию приложения для Android по ссылке");}
+    //            : if (clientOS == 0) {System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
+    //              else {System.out.println("Установите блегченную версию приложения для Android по ссылке");}
+
+    //Задача 3
+        int year=2300;
+        int kratnost4=year%4;
+        int kratnost100=year%100;
+        int kratnost400=year%400;
+
+        if (kratnost4 == 0 && kratnost100 != 0 || kratnost400 == 0) {System.out.println(year + " год является високосным");}
+        else {System.out.println(year + " год не является високосным");}
+
+    //Задача 4
+        int deliveryDistance = 110;
+        int deliveryDays;
+
+        if (deliveryDistance<20) {deliveryDays = 1;}
+        else if (deliveryDistance>=20&&deliveryDistance<60) {deliveryDays=2;}
+        else if(deliveryDistance>=60&&deliveryDistance<100) {deliveryDays=3;
+                                                             System.out.println("Потребуется дней: " +deliveryDays);}
+        else {System.out.println("Доставки нет");}
+
+    //Задача 5
+
+        int monthNumber=112;
+            switch (monthNumber){
+            case 1: System.out.println("Зима");
+                break;
+            case 2: System.out.println("Зима");
+                    break;
+            case 12: System.out.println("Зима");
+                    break;
+            case 3: System.out.println("Весна");
+                break;
+            case 4: System.out.println("Весна");
+                    break;
+            case 5: System.out.println("Весна");
+                    break;
+            case 6: System.out.println("Лето");
+                    break;
+            case 7: System.out.println("Лето");
+                    break;
+            case 8: System.out.println("Лето");
+                    break;
+            case 9: System.out.println("Осень");
+                    break;
+            case 10: System.out.println("Осень");
+                    break;
+            case 11: System.out.println("Осень");
+                    break;
+            default: System.out.println("Такого месяца не существует");}
+
+
+
+
+
     }
 }
